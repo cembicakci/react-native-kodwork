6c75e74c7cecfb8229f523d799b5aa8adbfd65ae
Cem Bıçakcı <cmbicakci@gmail.com>
2023-02-11 17:34:40 +0300

    installed drawer navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Jobs from './pages/Jobs';
-import { Detail } from './pages/Detail';
-
+import Detail from './pages/Detail';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
-function App() {
+const Home = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Jobs" component={Jobs} />
+      <Stack.Screen name="Detail" component={Detail} />
+    </Stack.Navigator>
+  )
+}
+
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Jobs" component={Jobs} />
-        <Stack.Screen name="Detail" component={Detail}/>
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
-
-export default App;
